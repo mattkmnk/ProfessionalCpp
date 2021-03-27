6ca6ea090235b9ea1769b28311bd4a5f244c4e91
Mateusz Kaminski <fressekk@gmail.com>
2021-03-27 16:04:16 +0100

    SpreadsheetCell modification

--- Part_III/SpreadsheetCell.cpp
+++ Part_III/SpreadsheetCell.cpp
@@ -1,9 +1,48 @@
 #include "SpreadsheetCell.h"
+#include <iostream>
+#include <sstream>
+
+SpreadsheetCell::SpreadsheetCell() : mValue(0) 
+{ }
+
+SpreadsheetCell::SpreadsheetCell(double initialValue) {
+    setValue(initialValue);
+}
+
+SpreadsheetCell::SpreadsheetCell(const std::string& initialValue) {
+    setString(initialValue);
+}
 
 void SpreadsheetCell::setValue(double inValue) {
     mValue = inValue;
+    mString = doubleToString(mValue);
 }
 
 double SpreadsheetCell::getValue() const {
     return mValue;
+}
+
+void SpreadsheetCell::setString(const std::string& inString) {
+    mString = inString;
+    mValue = stringToDouble(mString);
+}
+
+const std::string& SpreadsheetCell::getString() const {
+    return mString;
+}
+
+std::string SpreadsheetCell::doubleToString(double inValue) const{
+    std::ostringstream ostr;
+    ostr << inValue;
+    return ostr.str();
+}
+
+double SpreadsheetCell::stringToDouble(const std::string& inString) const {
+    double temp;
+    std::istringstream istr(inString);
+    istr >> temp;
+    if(istr.fail() || !istr.eof()) {
+        return 0;
+    }
+    return temp;
 }
--- Part_III/main.cpp
+++ Part_III/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "SpreadsheetCell.h"
 
 int main() {
-
+    SpreadsheetCell myCell, anotherCell;
+    myCell.setValue(6);
+    anotherCell.setString("3.2");
+    std::cout << "Cell 1: " << myCell.getValue() << '\n';
+    std::cout << "Cell 2: " << anotherCell.getValue() << '\n'; 
 }
